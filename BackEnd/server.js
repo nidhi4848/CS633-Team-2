cde3c68b13c32ed679a77c48f14c8d4e6cd5e207
sergiok10 <khalil.sergio@hotmail.com>
2024-11-06 15:32:09 -0500

    fix bug

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,11 +3,14 @@ import { postRoutes } from './routes/postRoutes.js';
 import { userRoutes } from './routes/userRoutes.js';
 import { searchRoutes } from './routes/searchRoutes.js';
 import { fridgeRoutes } from './routes/fridgeRoutes.js';
+import cors from 'cors';
 import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
