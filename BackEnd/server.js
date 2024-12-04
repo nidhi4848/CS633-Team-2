cebef8398d93ff4996286389acd34099061711c5
sergiok10 <khalil.sergio@hotmail.com>
2024-12-04 14:43:29 -0500

    Update server to listen on 0.0.0.0

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -23,7 +23,7 @@ app.use('/api/day', dayRoutes);
 
 mongoose.connect('mongodb+srv://sergiok:Ivyleague123@cluster0.yqwum.mongodb.net', { dbName: 'demo_db' }).then(() => {
     console.log('Connected to MongoDB');
-    app.listen(4000, 'localhost', () => {
+    app.listen(4000, '0.0.0.0', () => {
         console.log('listening on port 4000');
     });
 }).catch(err => {
