6ecfce4d22f081f7976dc25223ca2017d2f76bd5
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2024-12-05 15:22:35 -0500

    CalenderPage(Updated by Paridhi)

--- app/meal-plan/calendarpage.tsx
+++ app/meal-plan/calendarpage.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-// import { View, Text, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Calendar } from 'react-native-calendars';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet, TextInput, FlatList, Modal } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import BottomNav from '@/components/BottomNav';
 
 const CalendarPage = () => {
@@ -13,6 +12,7 @@ const CalendarPage = () => {
     const formattedDate = day.dateString; // Format is already YYYY-MM-DD
     router.push(`/meal-plan/${formattedDate}`);
   };
+
   const handleNavigation = (tab) => {
     switch (tab) {
       case 'Home':
@@ -38,22 +38,26 @@ const CalendarPage = () => {
   return (
     <View style={styles.pageContainer}>
       <Text style={styles.title}>Planner</Text>
-      <Calendar
-        onDayPress={handleDateClick}
-        markedDates={{
-          [selectedDate]: { selected: true, selectedColor: '#00adf5' },
-        }}
-        theme={{
-          selectedDayBackgroundColor: '#00adf5',
-          todayTextColor: '#00adf5',
-          arrowColor: '#00adf5',
-        }}
-      />
+      <View style={styles.calendarContainer}>
+        <Calendar
+          onDayPress={handleDateClick}
+          markedDates={{
+            [selectedDate]: { selected: true, selectedColor: '#00adf5' },
+          }}
+          theme={{
+            selectedDayBackgroundColor: '#00adf5',
+            todayTextColor: '#00adf5',
+            arrowColor: '#00adf5',
+          }}
+        />
+      </View>
       {/* Bottom Navigation */}
-      <BottomNav
-        items={['Home', 'Pantry', 'Meals', 'Recipes', 'Profile']}
-        onNavigate={handleNavigation}
-      />
+      <View style={styles.bottomNavContainer}>
+        <BottomNav
+          items={['Home', 'Pantry', 'Meals', 'Recipes', 'Profile']}
+          onNavigate={handleNavigation}
+        />
+      </View>
     </View>
   );
 };
@@ -61,18 +65,27 @@ const CalendarPage = () => {
 const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
-    justifyContent: 'center',
-    // alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#f5f5f5',
   },
   title: {
     fontSize: 36,
     fontWeight: 'bold',
+    marginTop: 40,
     marginBottom: 10,
     color: '#333',
     textAlign: 'center',
   },
-
+  calendarContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  bottomNavContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
 });
 
 export default CalendarPage;
