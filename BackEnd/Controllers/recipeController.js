089f837293e8858c261583b06422265a0db93331
sergiok10 <khalil.sergio@hotmail.com>
2024-12-12 11:55:15 -0500

    Refactor deleteRecipe function to use findByIdAndDelete and improve success message

--- BackEnd/Controllers/recipeController.js
+++ BackEnd/Controllers/recipeController.js
@@ -52,15 +52,15 @@ const getUserRecipes = async(req, res) => {
 
 const deleteRecipe = async (req, res) => {
     try {
-        const recipe = await Recipe.findById(req.params.recipeId);
+        const recipe = await Recipe.findByIdAndDelete(req.params.recipeId);
         if (!recipe) {
             return res.status(404).json({ error: 'Recipe not found' });
         }
-        await recipe.delete();
-        res.status(200).json({ message: 'Recipe deleted' });
+
+        res.status(200).json({ message: 'Recipe deleted successfully' });
 
         // Log the response
-        console.log({ message: 'Recipe deleted' });
+        console.log({ message: 'Recipe deleted successfully' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: error.message });
