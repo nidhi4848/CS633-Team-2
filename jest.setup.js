bb7c61569f2f970c7a8384358c128ee9d016e8c2
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2024-12-13 12:23:48 -0500

    Fridge_test

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,21 @@
 jest.mock('@react-native-async-storage/async-storage', () =>
-    require('@react-native-async-storage/async-storage/jest/async-storage-mock')
-  );
-  
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+jest.mock('@expo/vector-icons', () => ({
+  MaterialIcons: () => 'MockedMaterialIcons',
+  AntDesign: () => 'MockedAntDesign',
+}));
+jest.mock('expo-router', () => ({
+  router: {
+    push: jest.fn(),
+    replace: jest.fn(),
+    back: jest.fn(),
+  },
+}));
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+    query: {},
+  }),
+}));
